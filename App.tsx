223ce3dcd583459576ef2489f0bc650f64cf9220
Denis Pavlovic <contact@denispavlovic.com>
2023-06-27 18:53:29 +0200

    Added client loadMedia

--- App.tsx
+++ App.tsx
@@ -1,11 +1,25 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import GoogleCast, { CastButton, useCastState } from "react-native-google-cast";
+import { CastButton, useRemoteMediaClient } from 'react-native-google-cast'
 
 export default function App() {
+  const client = useRemoteMediaClient()
+
+  if (client) {
+    // Send the media to your Cast device as soon as we connect to a device
+    // (though you'll probably want to call this later once user clicks on a video or something)
+    client.loadMedia({
+      mediaInfo: {
+        contentUrl:
+          'https://commondatastorage.googleapis.com/gtv-videos-bucket/CastVideos/mp4/BigBuckBunny.mp4',
+        contentType: 'video/mp4',
+      },
+    })
+  }
+
   return (
     <View style={styles.container}>
-      <CastButton style={{ tintColor: "black", height: 48, width: 48 }} />
+      <CastButton style={{ width: 24, height: 24, tintColor: 'black' }} />
       <Text>Click on the Cast button</Text>
       <StatusBar style="auto" />
     </View>
