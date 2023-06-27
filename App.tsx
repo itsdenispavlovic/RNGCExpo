4a87e88528ced344a29efc6088b58799921b4fa2
Denis Pavlovic <contact@denispavlovic.com>
2023-06-27 20:12:24 +0200

    added useEffect

--- App.tsx
+++ App.tsx
@@ -1,22 +1,25 @@
 import { Button, StyleSheet, View, Text } from "react-native";
 import GoogleCast, { CastButton, useRemoteMediaClient, CastState, useCastState } from "react-native-google-cast";
 import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
 
 export default function App() {
   const client = useRemoteMediaClient();
   const castState = useCastState();
 
-  if (client) {
-    // Send the media to your Cast device as soon as we connect to a device
-    // (though you'll probably want to call this later once user clicks on a video or something)
-    client.loadMedia({
-      mediaInfo: {
-        contentUrl:
-          'https://commondatastorage.googleapis.com/gtv-videos-bucket/CastVideos/mp4/BigBuckBunny.mp4',
-        contentType: 'video/mp4',
-      },
-    })
-  }
+  useEffect(() => {
+    if (client) {
+      // Send the media to your Cast device as soon as we connect to a device
+      // (though you'll probably want to call this later once user clicks on a video or something)
+      client.loadMedia({
+        mediaInfo: {
+          contentUrl:
+            'https://commondatastorage.googleapis.com/gtv-videos-bucket/CastVideos/mp4/BigBuckBunny.mp4',
+          contentType: 'video/mp4',
+        },
+      })
+    }
+  }, [client]);
 
   return (
     <View style={styles.container}>
